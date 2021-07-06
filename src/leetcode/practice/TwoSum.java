4981b81b9395cefb72061802fb546073486d571c
saikat <saikatkarmakar99@gmail.com>
2021-07-06 13:42:21 +0530

    TwoSum code complete

--- src/leetcode/practice/TwoSum.java
+++ src/leetcode/practice/TwoSum.java
@@ -1,4 +1,22 @@
 package leetcode.practice;
 
+import java.io.IOException;
+
 public class TwoSum {
+    public static void main(String[] args)throws IOException {
+        int nums[]={2,7,11,15};
+        int target;
+        target = 26;
+
+        for (int i=0;i<nums.length-1;i++){
+            for (int j=i+1;j<nums.length;j++){
+                int sum=nums[i]+nums[j];
+                if(sum==target){
+                    System.out.println("Position is : "+i+" "+j);
+                }
+            }
+        }
+
+    }
+
 }
